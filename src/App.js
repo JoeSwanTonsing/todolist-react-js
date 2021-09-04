3c8cc635982fad640e5e9b0fe728ea229d0e7507
Suan Tonsing <suanasdf@gmail.com>
2021-09-04 22:13:27 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'; 
+
+import './app.css'; //Import the css we created
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    const [usrIn, setUsrIn] = useState(''); //Create state for capturing user input and initialize to empty string
+    const [list, setList] = useState([]); //State to maintain the user's todo list
+
+    //in state, the first value is used to access the value, and the second value is used to set the value.
+
+    //function to add todo item
+    function addItem() {
+        if (usrIn !== '') { //check if input is empty
+            const newItem = {
+                id: Math.random(), //generate random id
+                value: usrIn //actual input by user
+            }
+
+            const newlist = [...list]; //spread operator to get the current list of todos
+            newlist.push(newItem); //add the new added todo to the list
+            setList(newlist); //save the list
+            setUsrIn(''); //empty the input element
+        }
+    }
+
+    //when user types into the input, the entered value is captured here and stored to the state variable
+    function updateInput(event) {
+        setUsrIn(event.target.value); //set the input value to the state
+    }
+
+    //when user click on the todo item, remove that item
+    function removeItem(key) {
+        //get the current list
+        const currentList = [...list];
+
+        //from current list, filter and extract all items except the item that was clicked. We can do that by getting the id.
+        
+        const updatedList = currentList.filter(item => item.id !== key);
+        //if the key that was passed is not same as the item's id, then store it in the updatedList. If key===item.id, then it will omit.
+
+        setList(updatedList); //update the list
+    }
+
+    return (
+        <div className="container">
+            <div className="title-container">
+                <h3>To Do List</h3>
+            </div>
+            <div className="comp1-container">
+                <p>What do you want to do?</p>
+                <input type="text" id="inputTODO" name="inputTODO" placeholder="Enter To Do......" onChange={updateInput} value={usrIn} />
+                <button type="buttton" name="sub" onClick={addItem}>ADD</button>
+            </div>
+            {
+                list.length > 0 ?
+                    (
+                        <div className="comp2-container">
+                            <ul>
+                            {
+                                list.map(items => {
+                                    return (
+                                        <li key={items.id} onClick={() => removeItem(items.id)}>{items.value}<hr/></li>
+                                    )
+                                })
+                            }
+                            </ul>
+                        </div>
+                    )
+                    :
+                    (
+                        <div className="comp2-container">
+                            <p className="text-center">You have nothing to do? Such a lazy person!</p>
+                        </div>
+                    )
+            }
+
+        </div >
+    );
+};
+
+
 
 export default App;
---
+++ src/components/comp2.js
@@ -0,0 +1,14 @@
+import React from "react";
+import '../app.css';
+
+function Comp2() {
+    return(
+        <div className="comp2-container">
+            <ul>
+                <li>Todo Item</li>
+            </ul>
+        </div>
+    ); 
+};
+
+export default Comp2;
